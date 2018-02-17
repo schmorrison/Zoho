474de7c8fb33c949b22f8d2e2cc64c4069230d30
schmorrison <schmorrison@gmail.com>
2018-02-17 11:25:40 -0600

    expand the URL options encoding to allow a ',noencode' tag on options structs

--- crm/options.go
+++ crm/options.go
@@ -39,6 +39,7 @@ func encodeOptionsToURL(o optionEncoder, u *url.URL) error {
 
 		//split the tag on comma
 		tags := strings.Split(tag, ",")
+		encode := true
 		//	TAGS:
 		for _, a := range tags {
 			if a == "required" {
@@ -52,6 +53,8 @@ func encodeOptionsToURL(o optionEncoder, u *url.URL) error {
 					//Set the 'default' value for the URL item
 					value = strings.TrimPrefix(a, "default>")
 				}
+			} else if a == "noencode" {
+				encode = false
 			} else {
 				//if the underlying value of the field is NOT empty
 				if !reflect.DeepEqual(value, reflect.Zero(reflect.TypeOf(value)).Interface()) {
@@ -65,30 +68,54 @@ func encodeOptionsToURL(o optionEncoder, u *url.URL) error {
 		switch v := value.(type) {
 		case bool:
 			//encode to the URL
-			vals.Set(name, fmt.Sprintf("%t", v))
+			if encode {
+				vals.Set(name, fmt.Sprintf("%t", v))
+			}else {
+				u.RawQuery += fmt.Sprintf("%s=%t&",name, v)
+			}
 		case int:
-			vals.Set(name, fmt.Sprintf("%d", v))
+			if encode {
+				vals.Set(name, fmt.Sprintf("%d", v))
+			}else {
+				u.RawQuery += fmt.Sprintf("%s=%d&",name, v)
+			}
 		case float64:
-			vals.Set(name, fmt.Sprintf("%f", v))
+			if encode {
+				vals.Set(name, fmt.Sprintf("%f", v))
+			}else {
+				u.RawQuery += fmt.Sprintf("%s=%f&",name, v)
+			}
 		case string:
-			vals.Set(name, fmt.Sprintf("%s", v))
+			if encode {
+				vals.Set(name, fmt.Sprintf("%s", v))
+			}else {
+				u.RawQuery += fmt.Sprintf("%s=%s&",name, v)
+			}
 		case time.Time:
 			tm := time.Time(v)
 			if !tm.IsZero() {
-				vals.Set(name, fmt.Sprintf("%d-%d-%d %d:%d:%d",
-					v.Year(), v.Month(), v.Day(),
-					v.Hour(), v.Minute(), v.Second(),
-				))
+				if encode {
+					vals.Set(name, fmt.Sprintf("%d-%d-%d %d:%d:%d",
+						v.Year(), v.Month(), v.Day(),
+						v.Hour(), v.Minute(), v.Second(),
+					))
+				}else {
+					u.RawQuery += fmt.Sprintf("%s=%v&",name, v)
+				}
 			}
 		case crmData:
 			//get the items XML
-			vals.Set(name, v.writeXML())
+			if encode {
+				vals.Set(name, v.writeXML())
+			} else {
+				u.RawQuery += fmt.Sprintf("%s=%v&",name, v.writeXML())
+			}
 		}
 	}
 
 	vals.Set("version", "2")
 	vals.Set("newFormat", "1")
 
-	u.RawQuery = vals.Encode()
+	u.RawQuery += vals.Encode()
 	return nil
 }
