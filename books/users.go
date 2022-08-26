f2f6f499a7afac705480062a48ff53576d927a5a
Arne Zeising <arne.zeising@gmail.com>
2022-08-26 12:10:31 +0200

    Adapt struct

--- books/users.go
+++ books/users.go
@@ -30,68 +30,68 @@ func (c *API) GetCurrentUser() (data CurrentUserResponse, err error) {
 
 // CurrentUserResponse is the data returned by GetCurrentUser
 type CurrentUserResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int    `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
 	User    struct {
-		UserID   string `json:"user_id"`
-		Name     string `json:"name"`
+		UserID   string `json:"user_id,omitempty"`
+		Name     string `json:"name,omitempty"`
 		EmailIds []struct {
-			IsSelected bool   `json:"is_selected"`
-			Email      string `json:"email"`
-		} `json:"email_ids"`
-		Status   string `json:"status"`
-		UserRole string `json:"user_role"`
-		UserType string `json:"user_type"`
-		RoleID   string `json:"role_id"`
-		PhotoURL string `json:"photo_url"`
+			IsSelected bool   `json:"is_selected,omitempty"`
+			Email      string `json:"email,omitempty"`
+		} `json:"email_ids,omitempty"`
+		Status   string `json:"status,omitempty"`
+		UserRole string `json:"user_role,omitempty"`
+		UserType string `json:"user_type,omitempty"`
+		RoleID   string `json:"role_id,omitempty"`
+		PhotoURL string `json:"photo_url,omitempty"`
 		Role     struct {
 			Role struct {
-				RoleName    string `json:"role_name"`
+				RoleName    string `json:"role_name,omitempty"`
 				Permissions []struct {
-					FullAccess      bool   `json:"full_access"`
-					Entity          string `json:"entity"`
+					FullAccess      bool   `json:"full_access,omitempty"`
+					Entity          string `json:"entity,omitempty"`
 					MorePermissions []struct {
-						IsEnabled           bool   `json:"is_enabled"`
-						PermissionFormatted string `json:"permission_formatted"`
-						Permission          string `json:"permission"`
+						IsEnabled           bool   `json:"is_enabled,omitempty"`
+						PermissionFormatted string `json:"permission_formatted,omitempty"`
+						Permission          string `json:"permission,omitempty"`
 					} `json:"more_permissions,omitempty"`
 					ReportPermissions []struct {
 						Reports []struct {
-							FullAccess          bool   `json:"full_access"`
-							CanSchedule         bool   `json:"can_schedule"`
-							ReportConstant      string `json:"report_constant"`
-							CanShare            bool   `json:"can_share"`
-							IsExportEnabled     bool   `json:"is_export_enabled"`
-							ReportNameFormatted string `json:"report_name_formatted"`
-							IsScheduleEnabled   bool   `json:"is_schedule_enabled"`
-							CanExport           bool   `json:"can_export"`
-							CanAccess           bool   `json:"can_access"`
-						} `json:"reports"`
-						ReportGroupFormatted string `json:"report_group_formatted"`
-						ReportGroup          string `json:"report_group"`
+							FullAccess          bool   `json:"full_access,omitempty"`
+							CanSchedule         bool   `json:"can_schedule,omitempty"`
+							ReportConstant      string `json:"report_constant,omitempty"`
+							CanShare            bool   `json:"can_share,omitempty"`
+							IsExportEnabled     bool   `json:"is_export_enabled,omitempty"`
+							ReportNameFormatted string `json:"report_name_formatted,omitempty"`
+							IsScheduleEnabled   bool   `json:"is_schedule_enabled,omitempty"`
+							CanExport           bool   `json:"can_export,omitempty"`
+							CanAccess           bool   `json:"can_access,omitempty"`
+						} `json:"reports,omitempty"`
+						ReportGroupFormatted string `json:"report_group_formatted,omitempty"`
+						ReportGroup          string `json:"report_group,omitempty"`
 					} `json:"report_permissions,omitempty"`
-				} `json:"permissions"`
-				Description string `json:"description"`
-				DisplayName string `json:"display_name"`
-			} `json:"role"`
-			RoleID string `json:"role_id"`
-			Name   string `json:"name"`
-			Email  string `json:"email"`
-			Zuid   string `json:"zuid"`
-		} `json:"role"`
-		IsClaimant          bool          `json:"is_claimant"`
-		IsEmployee          bool          `json:"is_employee"`
-		Email               string        `json:"email"`
-		IsCustomerSegmented bool          `json:"is_customer_segmented"`
-		IsVendorSegmented   bool          `json:"is_vendor_segmented"`
-		IsAccountant        bool          `json:"is_accountant"`
-		CreatedTime         string        `json:"created_time"`
-		CustomFields        []interface{} `json:"custom_fields"`
+				} `json:"permissions,omitempty"`
+				Description string `json:"description,omitempty"`
+				DisplayName string `json:"display_name,omitempty"`
+			} `json:"role,omitempty"`
+			RoleID string `json:"role_id,omitempty"`
+			Name   string `json:"name,omitempty"`
+			Email  string `json:"email,omitempty"`
+			Zuid   string `json:"zuid,omitempty"`
+		} `json:"role,omitempty"`
+		IsClaimant          bool          `json:"is_claimant,omitempty"`
+		IsEmployee          bool          `json:"is_employee,omitempty"`
+		Email               string        `json:"email,omitempty"`
+		IsCustomerSegmented bool          `json:"is_customer_segmented,omitempty"`
+		IsVendorSegmented   bool          `json:"is_vendor_segmented,omitempty"`
+		IsAccountant        bool          `json:"is_accountant,omitempty"`
+		CreatedTime         string        `json:"created_time,omitempty"`
+		CustomFields        []interface{} `json:"custom_fields,omitempty"`
 		CustomFieldHash     struct {
-		} `json:"custom_field_hash"`
-		IsAssociatedForApproval  bool          `json:"is_associated_for_approval"`
-		IsAssociatedWithOrgEmail bool          `json:"is_associated_with_org_email"`
-		Branches                 []interface{} `json:"branches"`
-		DefaultBranchID          string        `json:"default_branch_id"`
-	} `json:"user"`
+		} `json:"custom_field_hash,omitempty"`
+		IsAssociatedForApproval  bool          `json:"is_associated_for_approval,omitempty"`
+		IsAssociatedWithOrgEmail bool          `json:"is_associated_with_org_email,omitempty"`
+		Branches                 []interface{} `json:"branches,omitempty"`
+		DefaultBranchID          string        `json:"default_branch_id,omitempty"`
+	} `json:"user,omitempty"`
 }
