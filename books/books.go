4f70f80f8d66aeed12f06d1ba03568f682b2b5f5
Arne Zeising <arne.zeising@gmail.com>
2022-08-26 11:49:41 +0200

    Add books New

--- books/books.go
+++ books/books.go
@@ -1,10 +1,34 @@
 package books
 
-import zoho "github.com/recap-technologies/Zoho"
+import (
+	"math/rand"
+	"time"
+
+	zoho "github.com/recap-technologies/Zoho"
+)
 
 // API is used for interacting with the Zoho CRM API
 // the exposed methods are primarily access to CRM modules which provide access to CRM Methods
 type API struct {
 	*zoho.Zoho
 	id string
+}
+
+// New returns a *books.API with the provided zoho.Zoho as an embedded field
+func New(z *zoho.Zoho) *API {
+	id := func() string {
+		var id []byte
+		keyspace := "abcdefghijklmnopqrutuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		for i := 0; i < 25; i++ {
+			source := rand.NewSource(time.Now().UnixNano())
+			rnd := rand.New(source)
+			id = append(id, keyspace[rnd.Intn(len(keyspace))])
+		}
+		return string(id)
+	}()
+
+	return &API{
+		Zoho: z,
+		id:   id,
+	}
 }
